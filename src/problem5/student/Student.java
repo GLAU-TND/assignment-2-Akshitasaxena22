b9a3a30e3011882af81696e3c787d97992de8709
Akshitasaxena22 <akshita.saxena_cs18@gla.ac.in>
2020-03-28 18:24:34 +0530

    implementation

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -6,5 +6,46 @@
  */
 package problem5.student;
 // to store student information and properties
+
 public class Student {
+    String Name;
+    int roll;
+    int acount;
+    int backlog;
+
+    public Student() {
+    }
+
+    ;
+
+    public Student(int roll, int backlog, int account) {
+        this.roll = roll;
+        this.acount = account;
+        this.backlog = backlog;
+    }
+
+    public int getAcount() {
+        return acount;
+    }
+
+    public int getBacklog() {
+        return backlog;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
 }
